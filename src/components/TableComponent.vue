2e12a20832e7c9274264be613764661f8384a720
miillaadddddd <milad7212@gmail.com>
2023-04-26 15:30:20 +0330

    add edit

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,57 +1,117 @@
 <template>
-  <div class=" p-4">
-    <ejs-grid
-      :dataSource="allData"
-      :allowPaging="true"
-      :allowSorting="true"
-      :pageSettings="pageSettings"
-    >
-      <e-columns>
-        <e-column
-          field="OrderID"
-          headerText="Order ID"
-          textAlign="Right"
-          width="90"
-        ></e-column>
-        <e-column
-          field="CustomerID"
-          headerText="Customer ID"
-          width="90"
-        ></e-column>
-        <e-column
-          field="Freight"
-          headerText="Freight"
-          textAlign="Right"
-          format="C2"
-          width="90"
-        ></e-column>
-      </e-columns>
-    </ejs-grid>
+  <div class="">
+    <div class="flex items-center gap-6 justify-center">
+      <input
+        type="text"
+        class="outline-blue-700 p-2 rounded-lg"
+        placeholder="نام"
+        v-model.lazy="rowData.name"
+      />
+      <input
+        type="text"
+        class="outline-blue-700 p-2 rounded-lg"
+        placeholder="نام خانوادگی"
+        v-model.lazy="rowData.family"
+      />
+      <input
+        type="text"
+        class="outline-blue-700 p-2 rounded-lg"
+        placeholder="کد کاربری"
+        v-model.lazy="rowData.code"
+      />
+      <button
+        @click="editData"
+        v-if="isEdit"
+        class="bg-blue-700 rounded-md text-white shadow-sm px-4 py-2 hover:scale-110 active:scale-95 transition-all duration-200 ease-out"
+      >
+        ویرایش
+      </button>
+      <button
+        @click="sendData"
+        v-else
+        class="bg-blue-700 rounded-md text-white shadow-sm px-4 py-2 hover:scale-110 active:scale-95 transition-all duration-200 ease-out"
+      >
+        افزودن
+      </button>
+    </div>
+    <div class="p-4">
+      <ejs-grid
+        :dataSource="allData"
+        :allowPaging="true"
+        :allowSorting="true"
+        :pageSettings="pageSettings"
+        @rowSelected="onRowSelected"
+      >
+        <e-columns>
+          <e-column
+            field="name"
+            headerText="نام"
+            textAlign="Right"
+            width="90"
+          ></e-column>
+          <e-column
+            field="family"
+            headerText="نام خانوادگی"
+            textAlign="Right"
+            width="90"
+          ></e-column>
+          <e-column
+            field="code"
+            headerText="کدکاربری"
+            textAlign="Right"
+            width="90"
+          ></e-column>
+        </e-columns>
+      </ejs-grid>
+    </div>
   </div>
-  
 </template>
 
 <script>
-import { Page, Sort ,Filter, Group} from "@syncfusion/ej2-vue-grids";
-import {mapGetters} from 'vuex'
+import { Page, Sort, Filter, Group } from "@syncfusion/ej2-vue-grids";
+import { mapGetters, mapActions } from "vuex";
 export default {
   data() {
     return {
-      
-      pageSettings: { pageSize: 5},
+      rowData: {
+        name: "",
+        family: "",
+        code: "",
+      },
+      pageSettings: { pageSize: 5 },
       provide: {
         grid: [Page, Sort, Filter, Group],
       },
-      
+      isEdit: false,
+      indexEdit: "",
     };
   },
-  computed:{
-    ...mapGetters(['allData'])
-  }
+  computed: {
+    ...mapGetters(["allData"]),
+  },
+  methods: {
+    ...mapActions(["setDataTable", "setEditRow"]),
+    sendData() {
+      this.setDataTable(this.rowData);
+      this.rowData = { name: "", family: "", code: "" };
+    },
+    onRowSelected(args) {
+      const selectedData = args.data;
+      this.rowData = selectedData;
+      this.isEdit = true;
+      this.indexEdit = args.rowIndex;
+    },
+    editData() {
+      let data={index:this.indexEdit,data:this.rowData}
+      this.setEditRow(data);
+      this.rowData = { name: "", family: "", code: "" };
+      this.isEdit = false;
+      this.indexEdit=''
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 @import "../../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
 </style>
--- src/store.js
+++ src/store.js
@@ -3,19 +3,30 @@ import { createStore } from "vuex";
 const store = createStore({
   state: {
     dataTable: [
-      { OrderID: 10248, CustomerID: "VINET", Freight: 32.38 },
-      { OrderID: 10249, CustomerID: "TOMSP", Freight: 11.61 },
-      { OrderID: 10250, CustomerID: "HANAR", Freight: 65.83 },
-      { OrderID: 10251, CustomerID: "VICTE", Freight: 41.34 },
-      { OrderID: 10252, CustomerID: "SUPRD", Freight: 51.3 },
-      { OrderID: 10253, CustomerID: "HANAR", Freight: 58.17 },
-      { OrderID: 10254, CustomerID: "CHOPS", Freight: 22.98 },
-      { OrderID: 10255, CustomerID: "RICSU", Freight: 148.33 },
-      { OrderID: 10256, CustomerID: "WELLI", Freight: 13.97 },
+      { code: 10248, name: "میلاد", family: "حسنی" },
+      { code: 10249, name: "سجاد", family: "حسنی" },
     ],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    updateDataTable(state, newData) {
+      state.dataTable = [newData, ...state.dataTable];
+    },
+    editDataTable(state,newData) {
+        
+        const newArr=[...state.dataTable];
+        newArr[newData.index]=newData.data
+        state.dataTable=newArr
+       
+    },
+  },
+  actions: {
+    setDataTable({ commit }, newData) {
+      commit("updateDataTable", newData);
+    },
+    setEditRow({commit},newData){
+        commit('editDataTable',newData)
+    }
+  },
   getters: {
     allData: (state) => state.dataTable,
   },
